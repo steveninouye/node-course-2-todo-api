c129814e998570cbf896955e4397088112368bd6
Steven Inouye <steveninouye@msn.com>
2018-03-13 22:18:06 -1000

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/Users', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne(
+  //   {
+  //     text: 'Something to do',
+  //     completed: false
+  //   },
+  //   (err, result) => {
+  //     if (err) {
+  //       return console.log('Unable to insert todo ', err);
+  //     }
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  //   }
+  // );
+
+  //Insert new doc into Users (name, age, location)
+  db.collection('Users').insertOne(
+    {
+      name: 'Steven Inouye',
+      age: 32,
+      location: 'Honolulu, Hawaii'
+    },
+    (err, result) => {
+      if (err) return console.log('Unable to add user ', err);
+      console.log(JSON.stringify(result.ops, undefined, 2));
+    }
+  );
+
+  db.close();
+});
